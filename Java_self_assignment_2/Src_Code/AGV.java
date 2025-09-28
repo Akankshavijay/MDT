200377e3eeebe5d453e67343ab412b175a5a9663
Akanksha Vijayvergiya <akanksha.vijayvergiya001@stud.fh-dortmund.de>
2025-09-28 15:47:22 +0100

    commit code for classes

--- Java_self_assignment_2/Src_Code/AGV.java
+++ Java_self_assignment_2/Src_Code/AGV.java
@@ -2,4 +2,41 @@ package Assignment;
 
 public class AGV {
 
+    private String id;
+    private double batteryLoad;
+    private double consumption;        
+    private int chargingTime;
+    private String position;
+    private float maxspeed;
+    private float actspeed;
+
+    public void setdata(String agvId, double battery, double cons,
+                        int charging, String pos, float maxSpd, float actSpd) {
+        id = agvId;
+        batteryLoad = battery;
+        consumption = cons;
+        chargingTime = charging;
+        position = pos;
+        maxspeed = maxSpd;
+        actspeed = actSpd;
+    }
+
+    public String getdata() {
+        return "AGV{id=" + id +
+               ", batteryLoad=" + batteryLoad +
+               ", consumption=" + consumption +
+               ", chargingTime=" + chargingTime +
+               ", position=" + position +
+               ", maxspeed=" + maxspeed +
+               ", actspeed=" + actspeed + "}";
+    }
+
+
+    public String getId() { 
+    	return id; 
+    }
+    
+    public double getConsumption() {
+    	return consumption; 
+    }
 }
--- Java_self_assignment_2/Src_Code/IndustrialProcess.java
+++ Java_self_assignment_2/Src_Code/IndustrialProcess.java
@@ -1,5 +1,59 @@
 package Assignment;
 
-public class IndustrialProcess {
+import java.util.ArrayList;
+import java.util.List;
 
+public class IndustrialProcess {
+    private String id;
+    private List<IOperation> operations;
+
+    public IndustrialProcess() {
+        operations = new ArrayList<>();
+    }
+
+
+    public void setId(String processId) { 
+    	id = processId;
+    }
+    
+    public void addOperation(IOperation op) { 
+    	operations.add(op); 
+    }
+
+    public int calculateTotalDuration() {
+        int total = 0;
+        for (IOperation op : operations) {
+            total = total + op.getduration();
+        }
+        return total;
+    }
+
+
+    public int countUniqueAGVs() {
+        java.util.List<String> uniqueIds = new java.util.ArrayList<>();
+        for (IOperation op : operations) {
+            for (AGV a : op.getResources()) {
+                boolean included = false;
+                for (String id : uniqueIds) {
+                    if (id.equals(a.getId())) {
+                    	included = true;
+                        break;
+                    }
+                }
+                if (!included) {
+                    uniqueIds.add(a.getId());
+                }
+            }
+        }
+        return uniqueIds.size();
+    }
+
+
+    public String print_indus() {
+        return "IndustrialProcess{id=" + id +
+               ", operations=" + operations.size() +
+               ", durationMinutes=" + calculateTotalDuration() +
+               ", uniqueAGVs=" + countUniqueAGVs() + "}";
+    }
 }
+
